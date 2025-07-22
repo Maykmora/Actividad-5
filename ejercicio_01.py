1761e20e77b2d1eba085352f0ab37432f56f3de0
Maykmora <maykmora12@gmail.com>
2025-07-22 09:37:11 -0600

    commit 2

--- ejercicio_01.py
+++ ejercicio_01.py
@@ -1,2 +1,12 @@
-print("\n--MENÚ DE OPCIONES--")
+print("Bienvenido al programa")
 while True:
+    print("\n--MENÚ DE OPCIONES--")
+    print("1.Ingresar lista de ventas")
+    print("2.Mostrar todas las ventas ingresadas")
+    print("3.Calcular la venta mas alta y la mas baja")
+    print("4.Calcular promedio de ventas")
+    print("5.Contar cuantos días superaron los Q.1000")
+    print("6. Buscar si una venta específica existe en la lista")
+    print("7.Clasificacion de cada venta")
+    print("8.Salir")
+    option=input("\nSeleccione una opcion: ")
