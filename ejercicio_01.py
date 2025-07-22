a962771c2917f1f7ead7e49088e2dc8e2c1f8e78
Maykmora <maykmora12@gmail.com>
2025-07-22 16:46:33 -0600

    commit 7

--- ejercicio_01.py
+++ ejercicio_01.py
@@ -1,9 +1,9 @@
 print("Bienvenido al programa")
+ventas = []
 while True:
-    ventas=[]
     print("\n--MENÚ DE OPCIONES--")
-    print("1.Ingresar lista de ventas")
-    print("2.Mostrar todas las ventas ingresadas")
+    print("1.Ingresar lista de ventas")   #Listo
+    print("2.Mostrar todas las ventas ingresadas")   #Listo
     print("3.Calcular la venta mas alta y la mas baja")
     print("4.Calcular promedio de ventas")
     print("5.Contar cuantos días superaron los Q.1000")
@@ -13,20 +13,33 @@ while True:
 
     match option:
         case "1":
-            while True:
-                venta=input("Ingrese la venta o 0 para salir:")
-                match venta:
-                    case "0":
-                        print("Saliendo")
-                        break
-                    case _:
-                        ventas.append(venta)
+            days=int(input("Ingrese el numero de días que desea agregar: "))
+            if days>0:
+                for i in range(days):
+                    while True:
+                        venta=input("Ingrese la venta del día:")
+                        if venta.isdigit():
+                            venta_int=int(venta)
+                            if venta_int>0:
+                                ventas.append(venta)
+                                break
+                            else:
+                                print("Valor invalido, intentelo de nuevo")
+                        else:
+                            print("Valor invalido, intentelo de nuevo")
+            else:
+                print("Error al ingresar datos")
         case "2":
             print("\nVentas ingresadas:")
             print(ventas)
 
         case "3":
             print("Calculando la venta mas alta y la más baja")
+            if ventas:
+                print("Máximo:", max(ventas))
+                print("Mínimo:", min(ventas))
+            else:
+                print("La lista está vacía")
 
         case "4":
             promedio=sum(ventas)/len(ventas)
